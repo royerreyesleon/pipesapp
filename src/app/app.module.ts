7eb65210a206f70470c9795afa9722087a9c3322
Royer Reyes León <royerleon7@gmail.com>
2022-07-13 14:18:54 -0500

    change lenguaje to spanish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 // import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+import localEs from '@angular/common/locales/es-MX';
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localEs);
 
 @NgModule({
     declarations: [
@@ -19,7 +23,9 @@ import { VentasModule } from './ventas/ventas.module';
         SharedModule,
         VentasModule
     ],
-    providers: [],
+    providers: [
+        {provide: LOCALE_ID, useValue: 'es-MX'}
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
