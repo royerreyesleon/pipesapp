ba9a7bafd6bb849d71cd2c5acede07673f71a0b6
Royer Reyes León <royerleon7@gmail.com>
2022-07-13 12:52:49 -0500

    add menubar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+      AppRoutingModule,
+    //   PrimeNgModule,
+      SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
