99cd6421bf4fa14af377a95861dfdb9ce426c2e0
Royer Reyes León <royerleon7@gmail.com>
2022-07-13 09:50:51 -0500

    pipes basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesapp';
+    nombre: string = 'RoyeR ReYeS';
+    valor: number = 1000;
+    obj: object = {
+        nombre: "royer"
+    }
+
+    cambiar(): void {
+        console.log(this.nombre);
+        console.log(this.valor);
+        console.log(this.obj);
+    }
 }
